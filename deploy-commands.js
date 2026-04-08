92f1ed495b5270faadcde8060379c7d525b18c42
daviemanuel123 <davi.xss244@gmail.com>
2026-04-08 13:36:29 -0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -15,5 +15,3 @@ client.on('interactionCreate', async interaction => {
     await interaction.reply('FUNCIONANDO ✅');
   }
 });
-
-client.login("MTQ5MDkwNzMwNjMxNjEzNjQ1OA.GaIDdc.aZW8mm9TD7xVE1RfrJGmdjsOu_D3hzzeOYOGlg");
